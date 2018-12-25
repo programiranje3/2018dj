28c78344fe3f2f92382439645e5bcf58c4bc535f
programiranje3 <devedzic@gmail.com>
2018-12-25 17:51:13 +0100

    Third Django class - generic views and forms.

--- music/admin.py
+++ music/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from music.models import Performer
+from music.models import Performer, Song
 
 admin.site.register(Performer)
+admin.site.register(Song)
--- music/models.py
+++ music/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 
 # Create your models here.
@@ -22,4 +24,28 @@ class Performer(models.Model):
 
     def get_absolute_url(self):
         return reverse('performer-detail', args=[str(self.id)])
+
+
+class Song(models.Model):
+
+    title = models.CharField(max_length=100)
+    performer = models.ForeignKey(Performer, on_delete=models.SET_NULL, blank=True, null=True)
+    time = models.TimeField(blank=True, null=True, default=datetime.time(minute=3, second=0))
+    release_date = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title + '\n' + \
+               '\tperformer: ' + str(self.performer) + '\n' + \
+               '\ttime: ' + self.format_time() + '\n' + \
+               '\trelease_date: ' + self.format_date()
+
+    def format_time(self):
+        return self.time.strftime('%#M:%S') if isinstance(self.time, datetime.time) else 'unknown'
+
+    def format_date(self):
+        return self.release_date.strftime('%b %d, %Y') if isinstance(self.release_date, datetime.date) else 'unknown'
+
+    def get_absolute_url(self):
+        return reverse('song-detail', args=[str(self.id)])
+
 
